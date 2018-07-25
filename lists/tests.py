cc6d9a1997ceb842d1277bc5497a138c5679d5a7
Mohammad Adil <mdadil2019@gmail.com>
2018-07-25 21:57:41 +0530

    Added dummy tested view

---
+++ lists/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_root_url_resolve_to_homepage_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
