496635b7bf4dcd5a29fe9892ad4b7a748c6b8256
Mohammad Adil <mdadil2019@gmail.com>
2018-08-04 06:06:36 +0530

    made changes in functional test to assert multiple items

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -46,12 +46,24 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # There is still a text box inviting him to add another item. He enters "Use peacock feathers to make a fly"
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         # The page updates again, and now shows both items on his lists
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+           '2: Use peacock feathers to make a fly', [row.text for row in rows]
+        )
 
 
         # Adil wonders that weather the site will remember the list. Then he sees that the site has generated a unique URL for his --
         # there is some explanatory text that effect.
+
+        self.fail("Finish the test!")
 
         # He visited that url - his to do list is still there
         # Satisfied, he goes back to sleep
