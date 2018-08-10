4434fe9b9c3d52d9d7abefa36f9f3bf83707502a
Mohammad Adil <mdadil2019@gmail.com>
2018-08-10 20:53:18 +0530

    Redirect after post and show all items in template

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -40,7 +40,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # When he hits enter, teh page updates and how the page lists "1: Buy peacock feathers" as an item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
 
         # We have occurance of same code more then two time to assert the strings so we will replace the code
         # with helper function so the concept is "Three strikes and refractor"
@@ -56,7 +56,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
 
         # The page updates again, and now shows both items on his lists
 
